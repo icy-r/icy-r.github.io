a76130caae8f8c3d463939f993b2c31d90999846
icy <asath12882@gmail.com>
2023-05-25 00:22:18 +0530

    updated with some cool effects

--- main/script.js
+++ main/script.js
@@ -17,4 +17,14 @@ fetch('https://api.ipify.org?format=json')
   })
   .catch(error => console.error(error));
 
-  
+  var navItems = document.querySelectorAll('.nav li');
+
+for (var i = 0; i < navItems.length; i++) {
+  navItems[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
