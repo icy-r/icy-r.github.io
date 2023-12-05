59296009b7ef310fa9e7523cfe9a8d76b51f1a0e
icy <asath12882@gmail.com>
2023-12-05 18:59:12 +0530

    Update loadprofile.js

--- js/loadprofile.js
+++ js/loadprofile.js
@@ -1,43 +1,37 @@
-const uid = sessionStorage.getItem('uid');
-//  console.log(uid);
+document.addEventListener('DOMContentLoaded', (event) => {
+    const uid = sessionStorage.getItem('uid');
+    const profile = document.querySelector('#header-profile-image');
 
-// console.log(name1);
-//setname to id name
+    if (uid) {
+        db.collection('users').doc(uid).get().then(doc => {
+            if (doc.exists) {
+                const userData = doc.data();
+                const userProfile = userData.profileImageUrl;
+                const name1 = userData.name;
+                const setname = document.getElementById("name");
+                setname.innerHTML = " " + name1;
+                profile.src = userProfile;
 
-const profile = document.querySelector('#header-profile-image');
-const funny = document.querySelector('#logo');
-funny.setAttribute('title', 'Hehe. if u click nothing will happen');
-
-if (uid) { // Check if uid is defined and not an empty string
-    db.collection('users').doc(uid).get().then(doc => {
-        if (doc.exists) {
-            const userData = doc.data();
-            const userProfile = userData.profileImageUrl;
-            const name1 = userData.name;
-            const setname = document.getElementById("name");
-            setname.innerHTML = " " + name1;
-            profile.src = userProfile;
-            // console.log(uid);
-            const logout = document.querySelector('#logout-btn');
-            logout.style.display = 'block';
-            logout.addEventListener('click', (e) => {
-                e.preventDefault();
-                auth.signOut().then(() => {
-                    console.log('user signed out');
-                    window.location.href = "../html/login.html";
+                const logout = document.querySelector('#logout-btn');
+                logout.style.display = 'block';
+                logout.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    auth.signOut().then(() => {
+                        console.log('user signed out');
+                        window.location.href = "../html/login.html";
+                    });
                 });
-            });
-        }
-    }).catch(err => {
-        console.log(err.message);
-    });
-} else {
-    const noname = document.querySelector('header-right-down');
-    noname.innerHTML = " ";
-    //create login button to profile
-    const login = document.createElement('a');
-    login.setAttribute('href', '../html/login.html');
-    login.innerHTML = "Login";
-    noname.appendChild(login);
-    console.log('uid is not defined or is an empty string');
-}
+            }
+        }).catch(err => {
+            console.log(err.message);
+        });
+    } else {
+        const noname = document.querySelector('header-right-down');
+        noname.innerHTML = " ";
+        const login = document.createElement('a');
+        login.setAttribute('href', '../html/login.html');
+        login.innerHTML = "Login";
+        noname.appendChild(login);
+        console.log('uid is not defined or is an empty string');
+    }
+});
